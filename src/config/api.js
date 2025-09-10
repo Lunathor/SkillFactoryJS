8fe0388b6612a4cf81ef17671aa429eb46b13a53
Nikita <mrcoopgmz@gmail.com>
2025-09-10 03:09:05 +0300

    Конфликт

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,6 @@
 // Конфигурация API
 export const API_CONFIG = {
-<<<<<<< HEAD
-=======
-  // Замените на ваш API ключ Google Books
->>>>>>> 97ea5af0a635b17e64839351ccee6b5badc84af5
+
   GOOGLE_BOOKS_API_KEY: 'AIzaSyBv5t9xefBc5hNNpY7QufyZwZLKM6ElEB0',
   
   // Базовые настройки
@@ -11,9 +8,5 @@ export const API_CONFIG = {
   MAX_RESULTS: 6,
   
   // Настройки для разработки
-<<<<<<< HEAD
   USE_API_KEY: true, 
-=======
-  USE_API_KEY: true, // Установите false, если хотите использовать API без ключа
->>>>>>> 97ea5af0a635b17e64839351ccee6b5badc84af5
 };
